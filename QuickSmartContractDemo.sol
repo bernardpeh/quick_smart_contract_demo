102e7e3774ac675c7786d1d4ac9d141774433dd8
Bernard Peh <bpeh@sitecritic.net>
2018-06-05 21:41:36 +1000

    update readme and comments

--- QuickSmartContractDemo.sol
+++ QuickSmartContractDemo.sol
@@ -19,20 +19,20 @@ contract QuickSmartContractDemo {
         _;
     }
     
-    // buyer creates contract with seller's address, due date and put in some funds
+    // buyer creates contract with seller's address, hash of secret password and put in some funds
     constructor(address _seller, bytes32 _hashed_password) payable public {
         buyer = msg.sender;
         seller = _seller;
         hashed_password = _hashed_password;
     }
     
-    // buyer acknowledges that service has been delivered and gives password to seller to get the funds
+    // buyer acknowledges that service has been delivered and gives password to seller. Sell can now withdraw funds
     function releaseFunds(uint _password) sellerOnly public {
         require(hashed_password == keccak256(abi.encodePacked(_password)),"password is wrong");
         seller.transfer(address(this).balance);
     }
     
-    // buyer can withdraw the funds if contract not yet activated and 
+    // buyer can withdraw the funds anytime as well. In real, we would subject this to certain conditions.
     function refund() buyerOnly public {
         selfdestruct(buyer);
     }
---
+++ README.md
@@ -0,0 +1,17 @@
+## Simple Ethereum Smart Contract Demo
+
+1. Installation
+
+```
+npm install
+```
+
+2. edit generate_password.js and generate a new bytes32 password
+
+```
+node generate_password.js
+``` 
+
+3. In remix, paste in the QuickSmartContractDemo.sol and deploy the contract
+
+4. Play around and enhance the contract

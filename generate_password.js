ee8dd12cb8021c2312a28d7a1a94d1d1cec87fcc
Bernard Peh <bpeh@sitecritic.net>
2018-06-05 21:52:31 +1000

    added comments

--- generate_password.js
+++ generate_password.js
@@ -2,4 +2,5 @@ var web3 = require('web3-utils')
 
 let passwd = '1111'
 
+// Note that web3.sha3 is different from solidity's keccak256 function. We have to use web3.soliditySha3 instead
 console.log(web3.soliditySha3(passwd))
